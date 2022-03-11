c1bca72707555875fabaf373eea51675b97414be
Andrew Manuel <andrewmanuel@Andrews-MacBook-Pro.local>
2022-03-11 12:17:28 -0500

    notion page imported, ty god

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("is this thing on?")
+
+let tab1 = document.querySelector("#tab1");
+let tab2 = document.querySelector("#tab2");
+let tab2 = document.querySelector("#tab3");
+let tab2 = document.querySelector("#tab4");
+
+
